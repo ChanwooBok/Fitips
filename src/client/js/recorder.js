4b9942f5711170e68a8c9a69042fcfdaeadf970d
Chanwoo <titoddd@naver.com>
2022-10-07 15:30:34 +0900

    video recording

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,19 +2,37 @@ const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
 let stream;
+let recorder;
+let videoFile;
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "my recording.webm"; // a has download attribute. It causes the browser to treat the linked URL as a download
+  document.body.appendChild(a);
+  a.click(); // 유저가 링크를 클릭한것과 같은 효과를 준다.
+};
 
 const handleStop = () => {
-  startBtn.innerText = "Start recording";
+  startBtn.innerText = "Download recording";
   startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
 };
 
 const handleStart = () => {
   startBtn.innerText = "Stop recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  const recorder = new MediaRecorder(stream);
-  console.log(recorder);
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+  recorder.ondataavailable = (e) => {
+    videoFile = URL.createObjectURL(e.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
 };
 
 const init = async () => {
