e544f90fcac2a85916369a1cd6bcdbc6b10dbdc7
Chanwoo <titoddd@naver.com>
2022-10-16 17:41:06 +0900

    comment

--- src/init.js
+++ src/init.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import "./db";
 import "./models/Video";
 import "./models/User";
+import "./models/Comment";
 //서버에 db를 연결시키고, db에서는 mongoose를 연결시켜서 model을 인식하게 한다.
 import app from "./server";
 
