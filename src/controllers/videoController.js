89815d98a8b0efa4257bb4ea70b9c229086c7a21
Chanwoo <titoddd@naver.com>
2022-10-19 20:21:24 +0900

    saving comment in video model

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,11 +31,12 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
+  console.log(video);
   if (!video) {
     return res.render("404", { pagetitle: "video not found" });
   }
-  console.log(video.owner.name);
+
   return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
 
@@ -174,6 +175,7 @@ export const createComment = async (req, res) => {
     owner: user._id,
     video: id,
   });
-
+  video.comments.push(comment._id);
+  video.save();
   return res.sendStatus(201);
 };
