01d8d2466e15d6dfd42729d30102ac9e83af5c48
Chanwoo <titoddd@naver.com>
2022-10-21 11:40:45 +0900

    comment

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -3,7 +3,7 @@ const form = document.getElementById("commentForm");
 
 const btn = form.querySelector("button");
 
-const handleSubmit = (event) => {
+const handleSubmit = async(event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
@@ -11,7 +11,7 @@ const handleSubmit = (event) => {
     return;
   }
   const videoId = videoContainer.dataset.id;
-  fetch(`/api/videos/${videoId}/comment`, {
+  await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json", // request에 대한 정보를 담고있는 headers / 백엔드야 json을 string처럼 만들어서 보내고 있다.라고 알려주는것.
