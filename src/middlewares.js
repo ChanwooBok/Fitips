abca3e196ba9025d0e5f3a989fcf007204e635e1
Chanwoo <titoddd@naver.com>
2022-11-26 00:02:05 +0900

    middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -29,11 +29,11 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-});
+}); // 프로필이미지 등록시, 파일사이즈크기를 제한함.
 
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
     fileSize: 10000000,
   },
-});
+}); // 영상등록시, 파일사이즈크기를 제한함.
